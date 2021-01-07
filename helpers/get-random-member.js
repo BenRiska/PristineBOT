6f0fd41c52174904b709a4fb85056d45b799f777
bentaylor <ben10taylor10@hotmail.com>
2021-01-07 19:33:17 +0000

    comment

--- helpers/get-random-member.js
+++ helpers/get-random-member.js
@@ -1,6 +1,7 @@
 const {members} = require("../fixtures")
 
 module.exports.getRandomMember = () => {
-    num = Math.floor(Math.random() * (members.length + 1) - 1) - 1
+    num = Math.floor(Math.random() * (members.length))
+    console.log(num)
     return members[num]
 }
