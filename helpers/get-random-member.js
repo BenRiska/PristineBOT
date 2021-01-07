4fc980b4d341a59ba9bcc7c7f3f6458e11a7b262
bentaylor <ben10taylor10@hotmail.com>
2021-01-07 19:21:55 +0000

    fixed getRandomMember error

--- helpers/get-random-member.js
+++ helpers/get-random-member.js
@@ -1,6 +1,6 @@
 const {members} = require("../fixtures")
 
 module.exports.getRandomMember = () => {
-    num = Math.floor(Math.random() * (members.length + 1) - 1)
+    num = Math.floor(Math.random() * (members.length + 1) - 1) - 1
     return members[num]
 }
