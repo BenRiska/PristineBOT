5f2e7c56656f84ec7f20e5f375d9c1dba03424aa
bentaylor <ben10taylor10@hotmail.com>
2021-01-25 18:00:57 +0000

    gif function

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 require("dotenv").config()
+const fetch = require("node-fetch")
 const {Client, MessageAttachment} = require("discord.js")
 const {getRandomValue} = require("./helpers/get-random-value")
 const {members, loliStatements} = require("./fixtures")
@@ -11,7 +12,11 @@ client.on("ready", () => {
     console.log("Our bot is ready to go!!!!")
   })
   
-client.on("message", msg => {
+client.on("message", messageHandler)
+
+
+  async function messageHandler(msg){
+
     if (msg.content == "I love pristine" || msg.content == "i love pristine") {
       msg.react("❤️")
       msg.channel.send("I love you all too! (except gobby)")
@@ -43,6 +48,24 @@ client.on("message", msg => {
       msg.react("❤️")
     }
 
-  })
+    if(msg.content[0] === "!"){
+
+      let query = msg.content.split("!")[1]
+
+      query = msg.content.split(" ")[0]
+
+      let url = `https://api.tenor.com/v1/search?q=${query}&key=${process.env.TENOR_KEY}&limit=8`
+
+      let response = await fetch(url)
+
+      let json = await response.json()
+
+      let index = Math.floor(Math.random() * (json.results.length))
+
+      msg.channel.send(json.results[index].url)
+
+    }
+
+}
   
 client.login(process.env.BOT_TOKEN)
--- helpers/get-random-value.js
+++ helpers/get-random-value.js
@@ -1,5 +1,4 @@
 module.exports.getRandomValue = (values) => {
     num = Math.floor(Math.random() * (values.length))
-    console.log(num)
     return values[num]
 }
